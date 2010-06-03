323c3ffe61138fd11eaa08d5982e031c68874cfb
unlogic <unlogic@b7ae9276-de66-4d77-b569-3de561d67412>
2010-06-03 21:56:24 +0000

    - Making progress

--- branches/1.1/src/se/unlogic/eagledns/plugins/RMIRemoteManagementPlugin.java
+++ branches/1.1/src/se/unlogic/eagledns/plugins/RMIRemoteManagementPlugin.java
@@ -10,12 +10,13 @@ import java.rmi.server.UnicastRemoteObject;
 import se.unlogic.eagledns.EagleLogin;
 import se.unlogic.eagledns.EagleManager;
 import se.unlogic.eagledns.LoginHandler;
+import se.unlogic.standardutils.numbers.NumberUtils;
 
 
 public class RMIRemoteManagementPlugin extends BasePlugin{
 
-	private String remotePassword;
-	private Integer remotePort;
+	private String password;
+	private Integer port;
 	private LoginHandler loginHandler;
 	
 	@Override
@@ -23,23 +24,23 @@ public class RMIRemoteManagementPlugin extends BasePlugin{
 
 		super.init(name);
 		
-		if (remotePassword == null || remotePort == null) {
+		if (password == null || port == null) {
 
 			throw new RuntimeException("Remote managed port and/or password not set, unable to start RMI remote managent plugin.");
 
 		} else {
 
-			log.info("Plugin " + this.name + " starting RMI remote management interface on port " + remotePort);
+			log.info("Plugin " + this.name + " starting RMI remote management interface on port " + port);
 
 			EagleManager eagleManager = new SystemInterfaceWrapper(systemInterface);
 			
-			this.loginHandler = new LoginHandler(eagleManager, this.remotePassword);
+			this.loginHandler = new LoginHandler(eagleManager, this.password);
 
 			try {
-				EagleLogin eagleLogin = (EagleLogin) UnicastRemoteObject.exportObject(loginHandler, remotePort);
-				UnicastRemoteObject.exportObject(eagleManager, remotePort);
+				EagleLogin eagleLogin = (EagleLogin) UnicastRemoteObject.exportObject(loginHandler, port);
+				UnicastRemoteObject.exportObject(eagleManager, port);
 
-				Registry registry = LocateRegistry.createRegistry(remotePort);
+				Registry registry = LocateRegistry.createRegistry(port);
 
 				registry.bind("eagleLogin", eagleLogin);
 
@@ -58,14 +59,14 @@ public class RMIRemoteManagementPlugin extends BasePlugin{
 		}		
 	}
 	
-	public void setRemotePassword(String remotePassword) {
+	public void setPassword(String remotePassword) {
 	
-		this.remotePassword = remotePassword;
+		this.password = remotePassword;
 	}
 
 	
-	public void setRemotePort(Integer remotePort) {
+	public void setPort(String remotePort) {
 	
-		this.remotePort = remotePort;
+		this.port = NumberUtils.toInt(remotePort);
 	}
 }
--- branches/1.1/src/se/unlogic/eagledns/resolvers/ASpoofingResolver.java
+++ branches/1.1/src/se/unlogic/eagledns/resolvers/ASpoofingResolver.java
@@ -1,103 +1,13 @@
 package se.unlogic.eagledns.resolvers;
 
-import org.xbill.DNS.ExtendedFlags;
-import org.xbill.DNS.Flags;
-import org.xbill.DNS.Message;
-import org.xbill.DNS.Name;
-import org.xbill.DNS.OPTRecord;
-import org.xbill.DNS.Rcode;
-import org.xbill.DNS.Record;
-import org.xbill.DNS.Section;
-import org.xbill.DNS.TSIG;
-import org.xbill.DNS.TSIGRecord;
 import org.xbill.DNS.Type;
 
-import se.unlogic.eagledns.EagleDNS;
-import se.unlogic.eagledns.Request;
-import se.unlogic.eagledns.plugins.BasePlugin;
-import se.unlogic.standardutils.numbers.NumberUtils;
 
-
-public class ASpoofingResolver extends BasePlugin implements Resolver{
-
-	protected String address = "127.0.0.1";
-	protected Long ttl = 300l;
-
-	public Message generateReply(Request request) throws Exception {
-
-		Message query = request.getQuery();
-
-		if(query.getQuestion().getType() == Type.A){
-
-			log.debug("Resolver " + name + " spoofing reply for " + query.getQuestion().getName());
-
-			TSIGRecord queryTSIG = query.getTSIG();
-			TSIG tsig = null;
-			if (queryTSIG != null) {
-				tsig = systemInterface.getTSIG(queryTSIG.getName());
-				if (tsig == null || tsig.verify(query, request.getRawQuery(), request.getRawQueryLength(), null) != Rcode.NOERROR) {
-					return null;
-				}
-			}
-
-			Message response = new Message(query.getHeader().getID());
-
-			response.getHeader().setFlag(Flags.QR);
-			if (query.getHeader().getFlag(Flags.RD)) {
-				response.getHeader().setFlag(Flags.RD);
-			}
-
-			Record record =  Record.fromString(Name.fromString("@",query.getQuestion().getName()), query.getQuestion().getType(), query.getQuestion().getDClass(), ttl, this.address, query.getQuestion().getName());
-
-			response.addRecord(record, Section.ANSWER);
-
-			OPTRecord queryOPT = query.getOPT();
-
-			int flags = 0;
-
-			if (queryOPT != null && (queryOPT.getFlags() & ExtendedFlags.DO) != 0) {
-				flags = EagleDNS.FLAG_DNSSECOK;
-			}
-
-			if (queryOPT != null) {
-				int optflags = (flags == EagleDNS.FLAG_DNSSECOK) ? ExtendedFlags.DO : 0;
-				OPTRecord opt = new OPTRecord((short) 4096, Rcode.NOERROR, (byte) 0, optflags);
-				response.addRecord(opt, Section.ADDITIONAL);
-			}
-
-			response.getHeader().setRcode(Rcode.NOERROR);
-
-			return response;
-		}
-
-		return null;
-	}
-
-	public void setAddress(String address) {
-
-		this.address = address;
-	}
-
-
-	public void setTtl(String ttlString) {
-
-		Long ttl = NumberUtils.toLong(ttlString);
-
-		if (ttl != null && ttl > 0) {
-
-			this.ttl = ttl;
-
-		} else {
-
-			log.warn("Invalid ttl " + ttlString + " specified!");
-		}
-	}
+public class ASpoofingResolver extends SpoofingResolver{
 
 	@Override
-	public void init(String name) throws Exception {
+	protected int getRecordType() {
 
-		super.init(name);
-		
-		log.info("Resolver " + name + " configured to spoof replies using address " + address);		
+		return Type.A;
 	}
 }
